de24d79ab4991041621eae28fafb35841675e756
Tyler Porter <tyler.b.porter@gmail.com>
2020-03-31 12:46:22 -0400

    autopep8 linting

--- bot.py
+++ bot.py
@@ -12,30 +12,32 @@ import traceback
 
 SetupHelper().setup_environment_variables()
 
-SUBMISSION_INDEX_MSG  = ('This comment is auto-generated to link your archive to your master subreddit.\n\n'
+SUBMISSION_INDEX_MSG = ('This comment is auto-generated to link your archive to your master subreddit.\n\n'
                         'Deleting this comment is VERY DANGEROUS and will break your archive.')
-ARCHIVE_INDEX_MSG     = ('This sticky is auto-generated to link your archive to your master subreddit.\n\n'
-                        'Deleting this submission is VERY DANGEROUS and will break your archive.')
+ARCHIVE_INDEX_MSG = ('This sticky is auto-generated to link your archive to your master subreddit.\n\n'
+                     'Deleting this submission is VERY DANGEROUS and will break your archive.')
 
-reddit                = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'],
-                                    client_secret=os.environ['REDDIT_CLIENT_SECRET'],
-                                    user_agent=os.environ['REDDIT_USER_AGENT'],
-                                    username=os.environ['REDDIT_USERNAME'],
-                                    password=os.environ['REDDIT_PASSWORD'])
-                     
+reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'],
+                     client_secret=os.environ['REDDIT_CLIENT_SECRET'],
+                     user_agent=os.environ['REDDIT_USER_AGENT'],
+                     username=os.environ['REDDIT_USERNAME'],
+                     password=os.environ['REDDIT_PASSWORD'])
+
 
 def run():
     if archive_needs_setup():
         set_up_archive()
 
     subreddit = reddit.subreddit(os.environ['REDDIT_SUBREDDIT'])
-    stream = praw.models.util.stream_generator(lambda **kwargs: submissions_and_comments(subreddit, **kwargs))
+    stream = praw.models.util.stream_generator(
+        lambda **kwargs: submissions_and_comments(subreddit, **kwargs))
     submission_type = praw.models.reddit.submission.Submission
     for post in stream:
         if isinstance(post, submission_type):
             archive_submission(post)
         else:
             archive_reply_to(post)
+
 
 def submissions_and_comments(subreddit, **kwargs):
     results = []
@@ -44,13 +46,15 @@ def submissions_and_comments(subreddit, **kwargs):
     results.sort(key=lambda post: post.created_utc, reverse=True)
     return results
 
+
 def archive_reply_to(post):
     submission_type = praw.models.reddit.submission.Submission
 
     if isinstance(post.parent(), submission_type):
         archive_submission = find_archive_submission(post.parent())
         archive_post = archive_submission.reply(post.body)
-        update_submission_index(post.id, archive_post.id, archive_submission.id)
+        update_submission_index(
+            post.id, archive_post.id, archive_submission.id)
 
         return archive_post
     else:
@@ -60,16 +64,21 @@ def archive_reply_to(post):
             parent = archive_reply_to(post.parent())
             archive_post = parent.reply(post.body)
 
-        update_submission_index(post.id, archive_post.id, archive_post.submission.id)
+        update_submission_index(post.id, archive_post.id,
+                                archive_post.submission.id)
         return archive_post
+
 
 def archive_submission(submission):
     if submission.is_self:
-        archived_submission = reddit.subreddit(os.environ['REDDIT_ARCHIVE']).submit(submission.title, selftext=submission.selftext)
+        archived_submission = reddit.subreddit(os.environ['REDDIT_ARCHIVE']).submit(
+            submission.title, selftext=submission.selftext)
     else:
-        archived_submission = reddit.subreddit(os.environ['REDDIT_ARCHIVE']).submit(submission.title, url=submission.url)
+        archived_submission = reddit.subreddit(os.environ['REDDIT_ARCHIVE']).submit(
+            submission.title, url=submission.url)
 
     update_subreddit_index(submission.id, archived_submission.id)
+
 
 def update_submission_index(comment_id, archive_comment_id, submission_id):
     submission = reddit.submission(id=submission_id)
@@ -84,10 +93,12 @@ def update_submission_index(comment_id, archive_comment_id, submission_id):
         else:
             existing_codes = existing_codes_matches[0]
 
-        new_index = build_index_comment(comment_id, archive_comment_id, existing_codes)
+        new_index = build_index_comment(
+            comment_id, archive_comment_id, existing_codes)
         index.edit(new_index)
     else:
         create_submission_index(comment_id, archive_comment_id, submission_id)
+
 
 def index_comment(submission):
     try:
@@ -95,13 +106,17 @@ def index_comment(submission):
     except IndexError:
         return None
 
+
 def create_submission_index(comment_id, archive_comment_id, submission_id):
     index_comment_body = build_index_comment(comment_id, archive_comment_id)
-    index_comment = reddit.submission(id=submission_id).reply(index_comment_body)
+    index_comment = reddit.submission(
+        id=submission_id).reply(index_comment_body)
     index_comment.mod.distinguish(sticky=True)
 
     print('Pausing for a few seconds to distinguish the index comment.')
-    time.sleep(10) # Pause for a few seconds, for some reason distinguishing a comment takes quite a long time.
+    # Pause for a few seconds, for some reason distinguishing a comment takes quite a long time.
+    time.sleep(10)
+
 
 def update_subreddit_index(submission_id, archive_submission_id):
     index = reddit.subreddit(os.environ['REDDIT_ARCHIVE']).sticky()
@@ -113,27 +128,33 @@ def update_subreddit_index(submission_id, archive_submission_id):
     else:
         existing_codes = existing_codes_matches[0]
 
-    new_index = build_master_index_comment(submission_id, archive_submission_id, existing_codes)
+    new_index = build_master_index_comment(
+        submission_id, archive_submission_id, existing_codes)
     index.edit(new_index)
+
 
 def build_index_comment(post_id, archive_id, existing_codes=''):
     base_message = SUBMISSION_INDEX_MSG + "\n\n$index: " + existing_codes
 
     return base_message + '{}:{};\n\n'.format(post_id, archive_id) + timestamp()
 
+
 def build_master_index_comment(post_id, archive_id, existing_codes=''):
     base_message = ARCHIVE_INDEX_MSG + "\n\n$index: " + existing_codes
 
     return base_message + '{}:{};\n\n'.format(post_id, archive_id) + timestamp()
 
+
 def find_archive_submission(submission):
     index = reddit.subreddit(os.environ['REDDIT_ARCHIVE']).sticky()
     old_index = index.selftext
     existing_codes = re.search(index_matcher(), old_index)[0]
-    submission_matcher = re.compile(r'(?<={}\:).*?(?=\;)'.format(submission.id))
+    submission_matcher = re.compile(
+        r'(?<={}\:).*?(?=\;)'.format(submission.id))
     submission_id = re.search(submission_matcher, existing_codes)[0]
 
     return reddit.submission(submission_id)
+
 
 def find_archive_comment(comment):
     try:
@@ -148,6 +169,7 @@ def find_archive_comment(comment):
     except Exception:
         return None
 
+
 def find_parent_comment(comment, submission):
     try:
         if isinstance(comment.parent(), Submission):
@@ -155,7 +177,8 @@ def find_parent_comment(comment, submission):
         else:
             index = index_comment(submission).body
             existing_codes = re.search(index_matcher(), index)[0]
-            comment_matcher = re.compile(r'(?<={}\:).*?(?=\;)'.format(comment.parent().id))
+            comment_matcher = re.compile(
+                r'(?<={}\:).*?(?=\;)'.format(comment.parent().id))
             parent_id = re.search(comment_matcher, existing_codes)[0]
 
             return reddit.comment(parent_id)
@@ -164,13 +187,17 @@ def find_parent_comment(comment, submission):
               'Posting to submission {} instead.'.format(submission.id))
         return submission
 
+
 def archive_cleaner():
     for submission in reddit.subreddit(os.environ['REDDIT_ARCHIVE']).top('all'):
         submission.delete()
 
+
 def set_up_archive():
-    submission = reddit.subreddit(os.environ['REDDIT_ARCHIVE']).submit('Archive Index for /r/{}'.format(os.environ['REDDIT_SUBREDDIT']), ARCHIVE_INDEX_MSG + timestamp())
+    submission = reddit.subreddit(os.environ['REDDIT_ARCHIVE']).submit(
+        'Archive Index for /r/{}'.format(os.environ['REDDIT_SUBREDDIT']), ARCHIVE_INDEX_MSG + timestamp())
     submission.mod.sticky()
+
 
 def archive_needs_setup():
     try:
@@ -179,11 +206,14 @@ def archive_needs_setup():
     except NotFound:
         return True
 
+
 def timestamp():
     return '\n\nLast updated: {}'.format(datetime.datetime.utcnow())
 
+
 def index_matcher():
     return re.compile(r'(?<=\$index\:\s).*(?=\n)')
+
 
 if __name__ == '__main__':
     while True:
--- env_sample.py
+++ env_sample.py
@@ -6,12 +6,12 @@ import os
 class SetupHelper():
     def setup_environment_variables(self):
         # Reddit instance details
-        os.environ['REDDIT_CLIENT_ID']     = 'REDDIT_CLIENT_ID'
+        os.environ['REDDIT_CLIENT_ID'] = 'REDDIT_CLIENT_ID'
         os.environ['REDDIT_CLIENT_SECRET'] = 'REDDIT_CLIENT_SECRET'
-        os.environ['REDDIT_USER_AGENT']    = 'REDDIT_USER_AGENT'
-        os.environ['REDDIT_USERNAME']      = 'REDDIT_USERNAME'
-        os.environ['REDDIT_PASSWORD']      = 'REDDIT_PASSWORD'
+        os.environ['REDDIT_USER_AGENT'] = 'REDDIT_USER_AGENT'
+        os.environ['REDDIT_USERNAME'] = 'REDDIT_USERNAME'
+        os.environ['REDDIT_PASSWORD'] = 'REDDIT_PASSWORD'
 
         # Bot instance details
-        os.environ['REDDIT_SUBREDDIT']     = 'REDDIT_SUBREDDIT'
-        os.environ['REDDIT_ARCHIVE']       = 'REDDIT_ARCHIVE'
+        os.environ['REDDIT_SUBREDDIT'] = 'REDDIT_SUBREDDIT'
+        os.environ['REDDIT_ARCHIVE'] = 'REDDIT_ARCHIVE'
